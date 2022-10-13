42787fe95b03af82b83782c0e3a1de0da67ccdfe
Mudit Gulgulia <mudit.gulgulia@sunarctechnologies.com>
2022-10-13 15:34:44 +0530

    InIt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgSelectModule } from '@ng-select/ng-select';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgSelectModule, FormsModule, ReactiveFormsModule],
-  providers: [],
+  imports: [BrowserModule, NgSelectModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  providers: [HttpClient],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
